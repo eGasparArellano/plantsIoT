b4c22123d3d6f8cd4b88f3bb402b9fc6854b6df0
Elias <eGasparArellano@gmail.com>
2020-11-10 18:54:14 -0600

    Components

---
+++ src/app/plant-details/plant-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plant-details',
+  templateUrl: './plant-details.component.html',
+  styles: []
+})
+export class PlantDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/plants-list/plants-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plants-list',
+  templateUrl: './plants-list.component.html',
+  styles: []
+})
+export class PlantsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
