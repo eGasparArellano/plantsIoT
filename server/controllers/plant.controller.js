13726d1be733bdaa146b6cec3117608c06161b87
Elias <eGasparArellano@gmail.com>
2020-11-17 20:03:53 -0600

    Plant details fix

--- server/controllers/plant.controller.js
+++ server/controllers/plant.controller.js
@@ -14,6 +14,18 @@ class PlantController {
         res.json(plants);
     }
 
+    async getPlantById(req, res) {
+        let query = {}          // Search by some criteria
+        let projection = "";    // Which fields are wanted
+        let options = {}        // Page or limit
+        
+        const id = req.params.id;
+        const docs = await Plant.getPlantById(id, projection, options);
+        const plant = JSON.parse(JSON.stringify(docs));
+
+        res.json(plant);
+    }
+
     async addPlant(req, res) {
         try {
             let plant = req.body;
--- src/app/plants-list/plant-list.service.ts
+++ src/app/plants-list/plant-list.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Subject } from 'rxjs';
+import { Plant } from 'src/models/Plant';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Subject } from 'rxjs';
 export class PlantListService {
   plantList: any[];
   plantSubject = new Subject<any>();
+  singlePlantSubject = new Subject<Plant>();
 
   constructor(private http: HttpClient) {
     this.loadPlants();
@@ -19,6 +21,15 @@ export class PlantListService {
       (data) => {
         this.plantList = data;
         this.plantSubject.next(this.getPlants());
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  getPlantById(id) {
+    this.http.get<Plant>(environment.url + '/api/plants/' + id).subscribe(
+      (data: Plant) => {
+        this.singlePlantSubject.next(data);
       },
       (err) => console.log(err)
     );
